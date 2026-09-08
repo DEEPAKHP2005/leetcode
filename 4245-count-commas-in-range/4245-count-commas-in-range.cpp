895b3fed483c9c254db9fba4c6cef57942fca15b
DEEPAKHP2005 <padashettid@gmail.com>
2026-09-08 12:23:08 +0530

    4245-COUNT-COMMAS-IN-RANGE Stats: Time: 0 ms (100%), Space: 8.5 MB (84.49%) - LeetHub

--- 4245-count-commas-in-range/4245-count-commas-in-range.cpp
+++ 4245-count-commas-in-range/4245-count-commas-in-range.cpp
@@ -3,13 +3,7 @@ public:
     int countCommas(int n) {
         if(n >= 1000){
             return n - 1000 + 1;
-        }
-        if(n >= 10000){
-            return n - 10000 + 1;
-        }
-        if(n >= 100000){
-            return n - 100000 + 1;
-        }
+            }
         return 0;
     }
 };
